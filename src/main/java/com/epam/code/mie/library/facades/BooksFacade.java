cdd2c300f65b5b2e1e01382ab55d4ae1ba39c75b
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-26 17:36:49 +0300

    Add Get Book By Name Endpoint and Integration Tests (#3)
    * Add findByName method to BooksRepository
    
    * Implement getBookByName method in BooksService
    
    * Add getBookByName method in BooksFacade
    
    * Add endpoint to fetch book by name in LibraryController
    
    * Add SQL script for test data
    
    * Add integration tests for LibraryController

--- src/main/java/com/epam/code/mie/library/facades/BooksFacade.java
+++ src/main/java/com/epam/code/mie/library/facades/BooksFacade.java
@@ -3,26 +3,27 @@ package com.epam.code.mie.library.facades;
 import com.epam.code.mie.library.dtos.BookDto;
 import com.epam.code.mie.library.mappers.BooksMapper;
 import com.epam.code.mie.library.services.BooksService;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BooksFacade {
 
-  private final BooksService booksService;
-  private final BooksMapper booksMapper;
+    private final BooksService booksService;
+    private final BooksMapper booksMapper;
 
-  @Transactional(readOnly = true)
-  public List<BookDto> getAllBooks() {
-    return booksMapper.toDtoList(booksService.getAllBooks());
-  }
+    @Transactional(readOnly = true)
+    public List<BookDto> getAllBooks() {
+        return booksMapper.toDtoList(booksService.getAllBooks());
+    }
 
-  @Transactional(readOnly = true)
-  public Optional<BookDto> getBookByName(String name) {
-    return booksService.getBookByName(name).map(booksMapper::toDto);
-  }
+    @Transactional(readOnly = true)
+    public Optional<BookDto> getBookByName(String name) {
+        return booksService.getBookByName(name).map(booksMapper::toDto);
+    }
 }
--- src/main/java/com/epam/code/mie/library/repositories/BooksRepository.java
+++ src/main/java/com/epam/code/mie/library/repositories/BooksRepository.java
@@ -3,7 +3,6 @@ package com.epam.code.mie.library.repositories;
 import com.epam.code.mie.library.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
