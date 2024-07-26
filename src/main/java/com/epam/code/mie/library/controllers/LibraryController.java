893250995e32d52b41f05f6787eadfbf7fe30180
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-26 17:12:51 +0300

    Add Swagger annotations for LibraryController endpoints

--- src/main/java/com/epam/code/mie/library/controllers/LibraryController.java
+++ src/main/java/com/epam/code/mie/library/controllers/LibraryController.java
@@ -2,6 +2,8 @@ package com.epam.code.mie.library.controllers;
 
 import com.epam.code.mie.library.dtos.BookDto;
 import com.epam.code.mie.library.facades.BooksFacade;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "Library Controller", description = "Controller for library endpoints")
 public class LibraryController {
 
   private final BooksFacade booksFacade;
 
   @GetMapping("/books")
+  @ApiOperation(value = "Get all books", notes = "Fetches all books available in the library")
   public List<BookDto> getAllBooks(){
     return booksFacade.getAllBooks();
   }
