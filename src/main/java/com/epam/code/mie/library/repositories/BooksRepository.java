139c8774ff80064cbc42134e822c37539401e225
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-26 17:27:16 +0300

    Add endpoint to get book by name (#2)
    * Add getBookByName method in BooksFacade
    
    * Implement getBookByName method in BooksService
    
    * Add findByName method in BooksRepository
    
    * Add endpoint to get book by name in LibraryController

--- src/main/java/com/epam/code/mie/library/repositories/BooksRepository.java
+++ src/main/java/com/epam/code/mie/library/repositories/BooksRepository.java
@@ -4,7 +4,9 @@ import com.epam.code.mie.library.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Long> {
-
+    Optional<Book> findByName(String name);
 }
--- src/main/java/com/epam/code/mie/library/services/BooksService.java
+++ src/main/java/com/epam/code/mie/library/services/BooksService.java
@@ -2,16 +2,26 @@ package com.epam.code.mie.library.services;
 
 import com.epam.code.mie.library.entities.Book;
 import com.epam.code.mie.library.repositories.BooksRepository;
-import java.util.List;
+import com.epam.code.mie.library.dtos.BookDto;
+import com.epam.code.mie.library.mappers.BooksMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BooksService {
-private final BooksRepository booksRepository;
 
-  public List<Book> getAllBooks() {
-    return booksRepository.findAll();
-  }
+    private final BooksRepository booksRepository;
+    private final BooksMapper booksMapper;
+
+    public List<BookDto> getAllBooks() {
+        return booksMapper.toBookDtoList(booksRepository.findAll());
+    }
+
+    public Optional<BookDto> getBookByName(String name) {
+        return booksRepository.findByName(name).map(booksMapper::toBookDto);
+    }
 }
