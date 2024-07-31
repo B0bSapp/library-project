92cc30d5e1d76df578cec54cbd486e98dadc7f36
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 12:40:43 +0300

    Annotate BookDto with @AuthorExists annotation.

---
+++ src/main/java/com/epam/code/mie/library/dto/BookDto.java
@@ -0,0 +1,14 @@
+package com.epam.code.mie.library.dto;
+
+import com.epam.code.mie.library.validation.AuthorExists;
+import lombok.Data;
+
+@Data
+public class BookDto {
+    private Long id;
+    @AuthorExists
+    private AuthorDto author;
+    private String name;
+    private String genre;
+    private String description;
+}
