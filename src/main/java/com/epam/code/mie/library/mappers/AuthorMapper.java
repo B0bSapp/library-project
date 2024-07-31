21d4055a6ed8208cf4334ac00ab27346a61ab18c
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 13:36:52 +0300

    Add toEntity method to AuthorMapper

--- src/main/java/com/epam/code/mie/library/mappers/AuthorMapper.java
+++ src/main/java/com/epam/code/mie/library/mappers/AuthorMapper.java
@@ -11,4 +11,13 @@ public class AuthorMapper implements Mapper<AuthorDto, Author> {
   public AuthorDto toDto(Author author) {
     return new AuthorDto(author.getName(), author.getLastName(), author.getSecondName(), List.of());
   }
+
+  public Author toEntity(AuthorDto authorDto) {
+    Author author = new Author();
+    author.setName(authorDto.getName());
+    author.setLastName(authorDto.getLastName());
+    author.setSecondName(authorDto.getSecondName());
+    // Assuming you might need to set books or other fields if applicable
+    return author;
+  }
 }
