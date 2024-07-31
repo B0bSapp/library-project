2c26403b4a22d36cb4ba1b8a0c37ab29553b5522
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 13:19:53 +0300

    Convert BookDto to record class

--- src/main/java/com/epam/code/mie/library/dtos/BookDto.java
+++ src/main/java/com/epam/code/mie/library/dtos/BookDto.java
@@ -1,9 +1,57 @@
 package com.epam.code.mie.library.dtos;
 
-public record BookDto(
-    Long id,
-    AuthorDto author,
-    String name,
-    String genre,
-    String description
-) {}
+public class BookDto {
+    private Long id;
+    private AuthorDto author;
+    private String name;
+    private String genre;
+    private String description;
+
+    public BookDto(Long id, AuthorDto author, String name, String genre, String description) {
+        this.id = id;
+        this.author = author;
+        this.name = name;
+        this.genre = genre;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AuthorDto getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDto author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
