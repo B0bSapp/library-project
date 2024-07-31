ad3c2247aa6f82b7b23e1a1db4ef30661227c06c
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 12:47:37 +0300

    Update AuthorExistsValidator class

--- src/main/java/com/epam/code/mie/library/validation/AuthorExistsValidator.java
+++ src/main/java/com/epam/code/mie/library/validation/AuthorExistsValidator.java
@@ -5,18 +5,38 @@ import com.epam.code.mie.library.dto.AuthorDto;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthorExistsValidator implements ConstraintValidator<AuthorExists, AuthorDto> {
 
     @Autowired
     private AuthorService authorService;
 
     @Override
+    public void initialize(AuthorExists constraintAnnotation) {
+        // Initialization code if needed
+    }
+
+    @Override
     public boolean isValid(AuthorDto authorDto, ConstraintValidatorContext context) {
-        return authorService.authorExists(
+        if (authorDto == null) {
+            return true; // consider null as valid, handle null-check in service if necessary
+        }
+
+        boolean exists = authorService.authorExists(
             authorDto.getName(),
             authorDto.getLastName(),
             authorDto.getSecondName()
         );
+
+        if (!exists) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Author does not exist")
+                   .addPropertyNode("author")
+                   .addConstraintViolation();
+        }
+
+        return exists;
     }
 }
