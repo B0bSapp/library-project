7ddf3e00744beaef04f8063b9d2c6ad56dfee614
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 12:46:57 +0300

    Remove validation code from LibraryController

--- src/main/java/com/epam/code/mie/library/controllers/LibraryController.java
+++ src/main/java/com/epam/code/mie/library/controllers/LibraryController.java
@@ -1,40 +1,44 @@
 package com.epam.code.mie.library.controllers;
 
-import com.epam.code.mie.library.dtos.BookDto;
-import com.epam.code.mie.library.facades.BooksFacade;
+import com.epam.code.mie.library.dto.BookDto;
+import com.epam.code.mie.library.services.BooksService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/api")
 @Tag(name = "Library Management System", description = "Operations related to library management")
 public class LibraryController {
 
-  private final BooksFacade booksFacade;
+    private final BooksService booksService;
 
-  @Operation(summary = "Get list of all books")
-  @GetMapping("/books")
-  public List<BookDto> getAllBooks(){
-    return booksFacade.getAllBooks();
-  }
+    public LibraryController(BooksService booksService) {
+        this.booksService = booksService;
+    }
 
-  @Operation(summary = "Get a book by name")
-  @GetMapping("/books/name")
-  public ResponseEntity<BookDto> getBookByName(@RequestParam String name) {
-    return booksFacade.getBookByName(name)
-        .map(ResponseEntity::ok)
-        .orElse(ResponseEntity.notFound().build());
-  }
+    @Operation(summary = "Get list of all books")
+    @GetMapping("/books")
+    public List<BookDto> getAllBooks(){
+        return booksService.getAllBooks();
+    }
 
-  @Operation(summary = "Add a new book")
-  @PostMapping("/books")
-  public ResponseEntity<BookDto> addBook(@RequestBody BookDto bookDto) {
-    BookDto createdBook = booksFacade.addBook(bookDto);
-    return ResponseEntity.ok(createdBook);
-  }
+    @Operation(summary = "Get a book by name")
+    @GetMapping("/books/name")
+    public ResponseEntity<BookDto> getBookByName(@RequestParam String name) {
+        return booksService.getBookByName(name)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @Operation(summary = "Add a new book")
+    @PostMapping("/books")
+    public ResponseEntity<BookDto> addBook(@RequestBody @Valid BookDto bookDto) {
+        BookDto createdBook = booksService.addBook(bookDto);
+        return ResponseEntity.ok(createdBook);
+    }
 }
