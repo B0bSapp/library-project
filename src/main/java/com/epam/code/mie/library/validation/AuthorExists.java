5f35dbdb1b89e97cba71d2357a945d968893954e
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 12:51:04 +0300

    Create AuthorExists annotation for validating author existence.

---
+++ src/main/java/com/epam/code/mie/library/validation/AuthorExists.java
@@ -0,0 +1,17 @@
+package com.epam.code.mie.library.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = AuthorExistsValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthorExists {
+    String message() default "Author does not exist";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
