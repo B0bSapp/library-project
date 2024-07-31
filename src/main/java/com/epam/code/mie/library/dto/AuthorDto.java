0f98a8368c8dbe3b7f75920ead51e8cbb51d36b8
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 12:47:49 +0300

    Update AuthorDto class

--- src/main/java/com/epam/code/mie/library/dto/AuthorDto.java
+++ src/main/java/com/epam/code/mie/library/dto/AuthorDto.java
@@ -1,10 +1,19 @@
 package com.epam.code.mie.library.dto;
 
 import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class AuthorDto {
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
     private String name;
+
+    @NotBlank(message = "Last name is mandatory")
+    @Size(max = 100, message = "Last name cannot exceed 100 characters")
     private String lastName;
+
+    @Size(max = 100, message = "Second name cannot exceed 100 characters")
     private String secondName;
 }
