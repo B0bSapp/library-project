b442248a3a4e14fad3d338693bb2e9c06ca44ae0
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 12:40:41 +0300

    Create AuthorDto class.

---
+++ src/main/java/com/epam/code/mie/library/dto/AuthorDto.java
@@ -0,0 +1,10 @@
+package com.epam.code.mie.library.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthorDto {
+    private String name;
+    private String lastName;
+    private String secondName;
+}
