ccb9e0267c65c48a5ef1671a709e43abfe5c44f9
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 14:16:12 +0300

    Add `AuthorExistsValidator` class to handle validation logic

---
+++ src/main/java/com/epam/code/mie/library/validation/AuthorExistsValidator.java
@@ -0,0 +1,23 @@
+package com.epam.code.mie.library.validation;
+
+import com.epam.code.mie.library.dtos.AuthorDto;
+import com.epam.code.mie.library.repositories.AuthorsRepository;
+lombok.RequiredArgsConstructor;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@RequiredArgsConstructor
+public class AuthorExistsValidator implements ConstraintValidator<AuthorExists, AuthorDto> {
+
+    private final AuthorsRepository authorsRepository;
+
+    @Override
+    public boolean isValid(AuthorDto authorDto, ConstraintValidatorContext context) {
+        if (authorDto == null) {
+            return false;
+        }
+        return authorsRepository.findByNameAndLastNameAndSecondName(
+                authorDto.name(), authorDto.lastName(), authorDto.secondName()).isPresent();
+    }
+}
