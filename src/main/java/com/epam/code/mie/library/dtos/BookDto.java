c1bddf5db20254f0279ae7c4226231260f6233a5
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 12:36:23 +0300

    Update BookDto class with AuthorExists annotation

--- src/main/java/com/epam/code/mie/library/dtos/BookDto.java
+++ src/main/java/com/epam/code/mie/library/dtos/BookDto.java
@@ -1,12 +1,14 @@
 package com.epam.code.mie.library.dtos;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.epam.code.mie.library.validation.AuthorExists;
+import lombok.Data;
 
-public record BookDto(
-    @Schema(description = "Name of the book") String name, 
-    @Schema(description = "Author of the book") AuthorDto author, 
-    @Schema(description = "Genre of the book") String genre, 
-    @Schema(description = "Description of the book") String description
-) {
-
+@Data
+public class BookDto {
+    private Long id;
+    @AuthorExists
+    private AuthorDto author;
+    private String name;
+    private String genre;
+    private String description;
 }
