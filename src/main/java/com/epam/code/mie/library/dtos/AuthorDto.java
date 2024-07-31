e966c0cafdf79a2d21d6dbd49f2e99fd32930d04
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 13:19:57 +0300

    Convert AuthorDto to record class

--- src/main/java/com/epam/code/mie/library/dtos/AuthorDto.java
+++ src/main/java/com/epam/code/mie/library/dtos/AuthorDto.java
@@ -2,9 +2,32 @@ package com.epam.code.mie.library.dtos;
 
 import java.util.List;
 
-public record AuthorDto(
-    String name,
-    String lastName,
-    String secondName,
-    List<BookDto> books
-) {}
+public class AuthorDto {
+    private final String name;
+    private final String lastName;
+    private final String secondName;
+    private final List<BookDto> books;
+
+    public AuthorDto(String name, String lastName, String secondName, List<BookDto> books) {
+        this.name = name;
+        this.lastName = lastName;
+        this.secondName = secondName;
+        this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public List<BookDto> getBooks() {
+        return books;
+    }
+}
