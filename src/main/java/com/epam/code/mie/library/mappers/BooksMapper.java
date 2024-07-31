6c995d92fee2a364262645160164ec5f8edf45dc
Oleg Khrystoforov <o.khristoforov@gmail.com>
2024-07-31 14:31:25 +0300

    Removed author mapping logic from BooksMapper.

--- src/main/java/com/epam/code/mie/library/mappers/BooksMapper.java
+++ src/main/java/com/epam/code/mie/library/mappers/BooksMapper.java
@@ -9,17 +9,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BooksMapper implements Mapper<BookDto, Book> {
 
-  private final AuthorMapper authorMapper;
-
   public BookDto toDto(Book entity) {
     return new BookDto(entity.getName(),
-        authorMapper.toDto(entity.getAuthor()), entity.getGenre(),
+        null, entity.getGenre(),
         entity.getDescription());
   }
 
   public Book toEntity(BookDto dto) {
     return new Book(dto.getName(),
-        authorMapper.toEntity(dto.getAuthor()), dto.getGenre(),
+        null, dto.getGenre(),
         dto.getDescription());
   }
 
